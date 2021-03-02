284c57495250a75bb1c7a390bc14c7d8fefbd73e
spiner90 <spiner90@gmail.com>
2021-03-03 00:30:12 +0300

    first commit

---
+++ src/main/java/com/test/FilmResource.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import org.eclipse.microprofile.graphql.*;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@GraphQLApi
+public class FilmResource {
+
+    @Inject
+    GalaxyService service;
+
+    @Query("allFilms")
+    @Description("Get all Films from a galaxy far far away")
+    public List<Film> getAllFilms() {
+        return service.getAllFilms();
+    }
+
+    @Mutation
+    public Hero createHero(Hero hero) {
+        service.addHero(hero);
+        return hero;
+    }
+
+    @Mutation
+    public Hero deleteHero(int id) {
+        return service.deleteHero(id);
+    }
+
+    @Query
+    @Description("Get a Films from a galaxy far far away")
+    public Film getFilm(@Name("filmId") int id) {
+        return service.getFilm(id);
+    }
+
+
+    public List<Hero> heroes(@Source Film film) {
+        return service.getHeroesByFilm(film);
+    }
+
+
+}
---
+++ src/main/java/com/test/LightSaber.java
@@ -0,0 +1,5 @@
+package com.test;
+
+public enum LightSaber {
+    RED, BLUE, GREEN
+}
